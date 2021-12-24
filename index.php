6ede650a6297d6a646145b8abf3515dd8cd2b8aa
leonTech254 <martinleontech23@gmail.com>
2021-12-24 12:24:05 +0300

    @leon_sep2021

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+require_once("navbar/index.php")
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title></title>
+</head>
+<body>
+   <div class="row">
+<div class="col-md circle"></div>  
+<div class="col-md jumbotron bg-none text-center">
+<h1 class="title-intro"><span class="span-chukua">chukua online</span></h1>
+<p class="short-info-landpage">
+chukua mtaani is a business platform that gives users opportunity to buy and sell good or services
+we also provide services like advertising both small and big businesses organitzations.To make your business established in the market please follow the link provide below.
+<div><a href="/projects/mitchelle/Account/contact@business" class="btn btn-outline-warning">establish your business <i class="fas fa-arrow-right"></i></a></div>
+</p>
+</div>  
+ <div class="col-md-5 bg-image">
+ </div>      
+     
+ <div class="col-md">
+    <div class="social-link">
+        <ul>
+        <li><a href="">facebook</<a><i class="fab fa-facebook"></i></li>
+        <li><a href="">twitter</<a><i class="fab fa-twitter"></i></li>
+        <li><a href="">instagram</<a><i class="fab fa-instagram"></i></li>
+        <li><a href="">telegram</<a><i class="fab fa-telegram"></i></li>
+    </ul>
+    </div>
+ </div>      
+   </div>
+</body>
+</html>
